88dc93eb9fac67b4a4117855da11a0a74f260a39
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2023-08-21 15:29:27 +0330

    Update MiniCaculator.py

--- MiniCaculator.py
+++ MiniCaculator.py
@@ -1,5 +1,18 @@
 from tkinter import*
 
+def sum():
+    n=int(entry1.get())
+    m=int(entry2.get())
+    label1['text']=n+m
+
+def div():
+    n=int(entry1.get())
+    m=int(entry2.get())
+    if m==0:
+        label1['text']='Cannot divide'
+    else:
+        label1['text']=n/m
+
 app=Tk()
 app.title('miniCalculator')
 app.geometry('420x240')
@@ -18,12 +31,12 @@ entry2.place(x=220,y=10)
 label1=Label(app,font='none 36',bg='#4445f7',fg='yellow',text='0')
 label1.place(x=10,y=70)
 #button
-btn1=Button(app,width='3',font='none 28',text='+')
+btn1=Button(app,width='3',font='none 28',text='+',command=sum)
 btn1.place(x=20,y=150)
 btn2=Button(app,width='3',font='none 28',text='-')
 btn2.place(x=120,y=150)
 btn3=Button(app,width='3',font='none 28',text='*')
 btn3.place(x=220,y=150)
-btn3=Button(app,width='3',font='none 28',text='/')
+btn3=Button(app,width='3',font='none 28',text='/',command=div)
 btn3.place(x=320,y=150)
 app.mainloop()

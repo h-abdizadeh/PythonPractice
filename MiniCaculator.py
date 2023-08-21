01b50c522de3f483de2ab2bc7749e52508a9e935
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2023-08-21 12:00:34 +0330

    Add files via upload

---
+++ MiniCaculator.py
@@ -0,0 +1,29 @@
+from tkinter import*
+
+app=Tk()
+app.title('miniCalculator')
+app.geometry('420x240')
+app.resizable(False,False)
+app['bg']='#4445f7'
+#entry
+#1
+entry1=Entry(app,font='none 25',width='10')
+entry1.insert(INSERT,'0')
+entry1.place(x=10,y=10)
+#2
+entry2=Entry(app,font='none 25',width='10')
+entry2.insert(INSERT,'0')
+entry2.place(x=220,y=10)
+#label
+label1=Label(app,font='none 36',bg='#4445f7',fg='yellow',text='0')
+label1.place(x=10,y=70)
+#button
+btn1=Button(app,width='3',font='none 28',text='+')
+btn1.place(x=20,y=150)
+btn2=Button(app,width='3',font='none 28',text='-')
+btn2.place(x=120,y=150)
+btn3=Button(app,width='3',font='none 28',text='*')
+btn3.place(x=220,y=150)
+btn3=Button(app,width='3',font='none 28',text='/')
+btn3.place(x=320,y=150)
+app.mainloop()

bf65865c0444f1bfe2b65fb2da50c28a224b6ed0
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2024-01-11 00:48:23 +0330

    Add files via upload

---
+++ Python05.py
@@ -0,0 +1,291 @@
+# 1
+# print('mohandesi farda')
+# print(5+12)
+# print(5-11)
+# print(3*4)
+# print(12/7)
+
+# print(2**4)
+
+# print(7>2)
+# print(6<3)
+
+# print(5+7)
+# print('5+7')
+# print('5'+'7')
+
+#2
+# name='ali'
+# family='ahmadi'
+# age=23
+
+# print(name)
+# print('name')
+# print(name+' '+family)
+# print(name+' '+family+' '+str(age))
+
+#3
+# print(str(3)+str(5))
+# print(int('3')+int('5'))
+# # print(int('ali'))#error
+# print(7/5)
+# print(int(7/5))
+# print(7%5)
+# print(8%2)
+# print(2)
+# print(float(2))
+# print(int(2.5))
+
+#4
+# name=input('enter fisrt name : ')
+# family=input('enter last name : ')
+
+# print('name is : '+name+' '+family)
+# print(f'name is : {name} {family}')
+
+#5
+# n=float(input('enter number : '))
+# m=float(input('enter number : '))
+
+# print(n+m)
+# print(n-m)
+# print(n*m)
+# print(n/m)
+
+#6
+# import winsound 
+# password='abc123'
+# userPass=input('enter password : ')
+
+# if password==userPass:
+#     print('YES')
+# else:
+#     print('NO')
+#     winsound.Beep(780,2000)
+
+#7
+# n=int(input('enter number : '))
+
+# if n<10:
+#     print(n+10)
+# else:
+#     print(n-10)
+
+#8
+# number=int(input('enter number : '))
+
+# if number%2==0:
+#     print('even')
+# else:
+#     print('odd')
+
+#9
+# color=input('enter color : ').lower()
+# # print(color.lower())
+# # print(color.upper())
+# # color=color.lower()
+
+# if color=='red':
+#     print('STOP')
+# elif color=='green':
+#     print('GO')
+# elif color=='yellow':
+#     print('SLOW')
+# else:
+#     import winsound
+#     winsound.Beep(2500,1000)
+
+#10
+# n=float(input('enter score : '))
+
+# if n>=18:
+#     print('A')
+# elif n>=15:
+#     print('B')
+# elif n>=12:
+#     print('C')
+# else:
+#     print('D')
+
+#11
+# password='abc123'
+# code=1234
+
+# #A
+# # userPass=input('enter password : ')
+# # if password==userPass:
+# #     userCode=int(input('enter code : '))
+# #     if code==userCode:
+# #         print('Ok')
+
+# #B
+# userPass=input('enter Password : ')
+# userCode=int(input('enter code : '))
+
+# # if password==userPass and code==userCode:
+# #     print('Ok')
+# if password==userPass or code==userCode:
+#     print('Ok')
+
+
+#12
+# mylist=['ali','sara','amir','maryam','parsa']
+# print(mylist)
+# print(*mylist)
+# print(*mylist,sep='-')
+# print(mylist[2])
+# # print(mylist[5])#error
+# print(mylist[-1])
+# print(len(mylist))
+
+#13
+# numbers=[3,7,-1,3,11]
+# print(max(numbers))
+# print(min(numbers))
+# print(sum(numbers))
+# print((numbers))
+# print(sum(numbers)/len(numbers))
+# print(numbers.count(3))
+# print(numbers.count(9))
+# print(numbers.index(7))
+# print(numbers.index(3))
+# numbers.sort(reverse=True)
+# print(numbers)
+
+#14
+# numbers=[3,7,-1,3,11]
+# print(sorted(numbers,reverse=True))
+# print(numbers)
+
+# numbers.reverse()
+# print(numbers)
+
+#15
+# names=[]
+# print(names)
+
+# names.append('ali')
+# names.append('sara')
+# names.append('amir')
+# # names.append(input('enter name : '))
+# names.insert(1,'amir')
+# names.insert(0,'maryam')
+# names.insert(len(names),'mehdi')
+# # print(names)
+# del names[3]
+# names.remove('amir')
+# temp=names.pop()
+# print(temp)
+# temp=names.pop(1)
+# print(temp)
+# print(names)
+
+#16
+# numbers=[3,9,-1,3,8]
+# #in
+# print(9 in numbers)
+# print(11 in numbers)
+# if -1 in numbers:
+#     numbers.remove(-1)
+
+# print(numbers)
+
+#17
+# list1=[]
+# list2=[2,5,9]
+
+# if list1:
+#     print(list1)
+# else:
+#     print('empty')
+
+# if list2:
+#     print(list2)
+# else:
+#     print('empty')
+
+#18
+# print(list(range(10)))
+# print(list(range(1,10)))
+# print(list(range(1,10,2)))
+# print(list(range(10,0,-1)))
+# print(list(range(10,0,-2)))
+
+#19
+# from random import randrange
+# print(randrange(1,10))
+# print(randrange(1000,10000))
+
+#20
+# from random import randrange
+# names=['tina','ali','amir reza','iliya',
+#        'mohammad amin','amir reza','parsa']
+# #1
+# index=randrange(0,len(names))
+# print(index)
+# print(names[index])
+# # #2
+# # index=randrange(0,len(names))
+# # print(index)
+# # print(names[index])
+# # #3
+# # index=randrange(0,len(names))
+# # print(index)
+# # print(names[index])
+
+#21
+# from random import randrange
+# names=['tina','ali','amir reza','iliya',
+#        'mohammad amin','amir reza','parsa']
+
+# for n in range(0,7):
+#     i=randrange(len(names))
+#     print(f"index {i} ==> {names[i]}")
+#     # print("index "+i+" ==> "+names[i])
+#     del names[i]
+
+#22 tuple
+# user=('ali','ahmadi',26)
+# print(user)
+# print(*user)
+# print(user[0])
+# # user.append('09112223344')#error
+# # user[3]='09112223344'#error
+# # user[1]='asadi'#error
+# print(*user)
+# print(type(user))
+# user2=('sara','amiri',24)
+# mylist=[user,user2]
+# print(*mylist,sep='\n')
+
+# inputTuple=(input('enter name : '), 
+#             input('enter family : '))
+# print(inputTuple)
+
+#23
+# user={'name':'ali',
+#       'family':'amiri',
+#       'age':32}
+
+# print(user)
+# print(*user)
+# print(user['family'])
+
+# user['phone']='0911'
+# print(user)
+
+# user['family']='sadeghi'
+# print(user)
+
+#24
+setA={5,8,3,12}
+setB={10,8,4,9}
+print(setA)
+print(setA.union(setB))
+print(setA.intersection(setB))
+print(setA.difference(setB))
+print(setB.difference(setA))
+setA.remove(5)
+setA.add(11)
+print(setA)
+print(max(setA))
---
+++ python04.py
@@ -0,0 +1,215 @@
+#1
+# print('hello')
+# print(7+5)
+# print(8-3)
+# print(6*2)
+# print(14/4)
+# print(5>3)
+# print(8<2)
+# print('ali'>'reza')
+# print('amir'>'ali')
+
+#2
+# n=5
+# m=7
+# print(n)
+# print('n')
+# print(n+m)
+# print('n+m')
+# print('n'+'m')
+# print("15"*3)
+
+#3
+# name=input('enter first name : ')
+# family=input('enter last name : ')
+# print(name+' '+family)
+
+#4
+# n=5
+# m=7
+# print(5+7)
+# print('result = '+str(5+7))
+# print(str(9)+str(3))
+# print(str(n)+str(m))
+# print(int(2.5)+int(3.75))
+# print(14/8)
+# print(int(14/8))
+# print(13%5)
+# print(12%6)
+# print(int(2))
+# print(float(2))
+
+#5
+# n=float(input('enter number : '))
+# m=float(input('enter number : '))
+# print(n+m)
+# print(n-m)
+# print(n*m)
+# print(n/m)
+
+#6
+# if 5>2:
+#     print('yes')
+# else:
+#     print('no')
+
+#7
+# password='abc123'
+# userPass=input('enter password : ')
+
+# if password==userPass:
+#     print('OK')
+# else:
+#     print('WRONG')
+
+#8
+# n=int(input('enter number : '))
+
+# if n%2==0:
+#     print('even')
+# else:
+#     print('odd')
+
+#9
+# number=int(input('enter number : '))
+
+# if number<10:
+#     print('one digit')
+# else:
+#     print('invalid')
+
+#10
+# color=input('enter color : ')
+
+# if color=='red':
+#     print('STOP')
+# elif color=='green':
+#     print('GO')
+# elif color=='yellow':
+#     print('SLOW')
+# else:
+#     print('ALARM')
+#     import winsound
+#     winsound.Beep(700,5000)
+
+#11
+# n=float(input('enter score : '))
+# p=input('enter project [yes/no] :')
+
+# # if n>=17 and p=='yes':
+# if n>15 or p=='yes':
+#     print('accept')
+# else:
+#     print('failed')
+
+#12
+# names=['melina','sepanta','amirhossein']
+# print(names)
+# print(*names)
+# print(*names,sep=',')
+# print(names[1])
+
+#13
+# from random import randrange
+# print(randrange(100))#0 -> 99
+# print(randrange(10,100))#10 -> 99
+
+#14
+# from random import randrange
+# names=['melina','sepanta','amirhossein']
+# i=randrange(0,3)
+# print(i)
+# print(names[i])
+
+#15
+# names=['ali','sara','amir','reza','maryam']
+# numbers=[12,-1,2,9,2,10]
+# print(names[3])
+# print(names[-1])
+# print(len(names))
+# print(len(numbers))
+# print(max(numbers))
+# print(min(numbers))
+# print(sum(numbers))
+# print(sorted(names))
+# print(sorted(numbers))
+# print(sorted(names,reverse=True))
+# print(sorted(numbers,reverse=True))
+
+#16
+# names=['ali','sara','amir','reza','maryam']
+# numbers=[12,-1,2,9,2,10]
+# # names.sort()
+# # print(names)
+# # numbers.sort()
+# # print(numbers)
+# # names.sort(reverse=True)
+# # print(names)
+# names.reverse()
+# print(names)
+
+#17 slice
+# names=['ali','sara','amir','reza','maryam']
+# numbers=[12,-1,2,9,2,10]
+# print(names[1:4])
+# print(names[:4])
+# print(names[2:])
+# print(names[:])
+
+#18
+# names=[]
+# print(type(names))
+# print(names)
+
+# names.append('narges')
+# # names.append(input('enter name : '))
+# names.append('amir')
+# names.append('reza')
+# names.insert(0,'ali')
+# names.insert(2,'sara')
+# names.insert(len(names),'maryam')
+# print(names)
+
+#19
+# names=['ali','amir','sara','maryam','amir']
+# print(names)
+
+# # del names[3]
+# # names.remove('ali')
+# # names.remove('Ali'.lower())#.upper()
+# # names.remove('amir')
+# # print(names.pop())
+# # n=names.pop()
+# # print(names)
+# # names.append(n)
+# print(names.pop(2))
+# print(names)
+
+#20
+# names=['ali','amir','sara','maryam','amir']
+# print('ali' in names)
+# print('narges' in names)
+
+# if 'narges' in names:
+#     names.remove('narges')
+
+# print(names)
+
+#21
+# text='hello world, python'
+# print('or' in text)
+# print('remove' in text)
+
+#22
+# names=[12,15,9]#names[]
+# if names:
+#     print(len(names))
+
+#23
+print(range(10))
+print(*range(10))
+print(*range(5,20))
+print(*range(10,50,5))
+print(*range(10,2,-1))
+print(*range(50,10,-5))
+print(*range(50,-50,-10))

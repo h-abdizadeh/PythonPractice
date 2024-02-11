649682ecc9e68534232a63b8de0f1353dd66f184
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2024-02-11 15:43:50 +0330

    Update python04.py

--- python04.py
+++ python04.py
@@ -206,10 +206,141 @@
 #     print(len(names))
 
 #23
-print(range(10))
-print(*range(10))
-print(*range(5,20))
-print(*range(10,50,5))
-print(*range(10,2,-1))
-print(*range(50,10,-5))
-print(*range(50,-50,-10))
+# print(range(10))
+# print(*range(10))
+# print(*range(5,20))
+# print(*range(10,50,5))
+# print(*range(10,2,-1))
+# print(*range(50,10,-5))
+# print(*range(50,-50,-10))
+
+####### review ######
+#1
+# # print('ali'*'reza')#error
+# print(2*'ali')
+
+#2
+# name='amir'
+# family='ahmadi'
+
+# print('user name '+name+' '+family+'...')
+# print(f'user name {name} {family}...')
+
+#3
+# print(-1*5)
+# print(-1*-5)
+# print(5>2)
+# print(not 5>2)
+# print(6>10)
+# print(not 6>10)
+
+#4
+# n=[3,5,2,-2]
+# print(max(n))
+# print(max(range(10,100,2)))
+
+# from random import randrange
+# print(randrange(10,100,2))
+
+#5
+# print(range(5,50,2))
+# print(list(range(5,56,2)))
+
+####### review ######
+
+#24 filter
+# score=[14,12,18,17,16,5,12,18,20,18,1,7,14,18,16,19,11,15]
+# #1
+# # f=filter(lambda n:n>=18,score)
+# # print(f)
+# # result=list(f)
+# # print(result)
+# # print(len(result))
+# # print(max(result))
+
+# #2 practice #1
+# print(score)
+# # f=list(filter(lambda n: n>=10,score))
+# # f=list(filter(lambda n: n<15,f))
+# # print(f)
+
+# #2
+# f=filter(lambda n:n>=10 and n<15,score)
+# print(list(f))
+
+#25 tuple
+# tuple1=('ali','amiri',35)
+# print(tuple1)
+# print(*tuple1)
+# print(tuple1[1])
+
+#26
+# user=(input('enter name : '),
+#       input('enter family'),
+#       input('enter phone : '))
+
+# print(user)
+
+#27
+# list1=[5,8,12]
+# print(list1)
+# print(tuple(list1))
+
+#28
+# user1=('reza','asadi')
+# user2=('sara','sadeghi')
+# listUser=[user1,user2]
+# print(listUser)
+# print(listUser[0])
+# print(listUser[0][1])
+# print(listUser[1][0])
+
+#29 dictionary
+# dict1={'name':'ali', 'family':'asadi'}
+# print(dict1)
+# print(*dict1)
+# print(dict1['family'])
+# print(dict1['family'])
+
+# dict1['age']=35
+# print(dict1)
+
+#30
+# dict1={'name': 'ali','age':37}
+# dict2={'name': 'reza','age':32}
+
+# myLsit=[dict1,dict2]
+# print(myLsit)
+# print(myLsit[0])
+# print(myLsit[1]['age'])
+
+#31 set
+# setA={1,2,5,7}
+# setB={3,5,9,7}
+# # print(setA)
+# # print(*setA)
+# print(list(setA)[2])
+# # print(setA.union(setB))
+# # print(setA.intersection(setB))
+# # print(setA.difference(setB))
+# # print(setB.difference(setA))
+
+# # #1
+# # u=setA.union(setB)
+# # print(u)
+# # i=setA.intersection(setB)
+# # # print(u-i)
+# # print(u.difference(i))
+
+# # #2
+# # print(setA.union(setB).difference(setA.intersection(setB)))
+
+#32
+myStr='hello world, python'
+for n in myStr:
+    if n=='o':
+        print(n.upper(),end='')
+    else:
+        print(n,end='')
+
+

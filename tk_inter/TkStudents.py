b1042acdbf099a7e4fb692512536423ffcb52f90
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2024-04-28 17:00:14 +0330

    Update TkStudents.py

--- tk_inter/TkStudents.py
+++ tk_inter/TkStudents.py
@@ -13,6 +13,22 @@ def SubmitName():
             file.write(f'{name}\n')
             entryName.delete(0,END)
 
+def ShowNames():
+    list1.delete(0,END)#clear listbox
+    with open(fileAddress) as file:#'r -> read file'
+        names=file.readlines()
+        for n in names:
+            list1.insert(END,n)
+            #list1.insert(0,n)
+
+def SelectStudent():
+    from random import randrange
+    with open(fileAddress) as file:
+        names=file.readlines()
+        index=randrange(len(names))
+        # list1.insert(0,names[index])
+        messagebox.showinfo('student',names[index])
+
 
 app=Tk()
 app.title('students list')
@@ -29,5 +45,23 @@ btnSubmit=Button(app,
                  text='submit',
                  command=SubmitName)
 btnSubmit.place(x=200,y=8)
+
+btnShow=Button(app,
+               text='show',
+               font='none 12',
+               command=ShowNames)
+btnShow.place(x=270,y=8)
+
+list1=Listbox(app,
+              font='none 12',
+              width=20,
+              height=10)
+list1.place(x=10,y=60)
+
+btnSelect=Button(app,
+                 font='none 32',
+                 text='?',
+                 command=SelectStudent)
+btnSelect.place(x=250,y=150)
 
 app.mainloop()

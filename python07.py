a2d91961200b39638d4d26ff05171729c54f28e2
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2024-08-07 17:38:42 +0330

    Update python07.py maharat

--- python07.py
+++ python07.py
@@ -327,10 +327,163 @@
 # print(randrange(-10,-1,1))
 
 #28
-from random import randrange
-names=['amir','sara','reza','maryam']
-n=randrange(len(names))
-print(names[n])
+# from random import randrange
+# names=['amir','sara','reza','maryam']
+# n=randrange(len(names))
+# print(names[n])
 
-###
-# print(randrange(len(['amir','sara','reza','maryam'])))
+#29
+# import random
+# print(random.sample(range(10),3))
+# print(random.sample(range(10),10))
+# print(random.randint(10,20))
+# print(random.choice(range(10)))
+# print(random.choice([1,8,9,-3]))
+# print(random.choices(range(10),k=2))
+# print(random.choices(range(10),k=10))
+# print(random.choices([1,8,9,-3],k=2))
+
+#30
+# mylist=[[1,5],[3,-1,8],5]
+# print(mylist)
+# print(*mylist)
+# print(mylist[0])
+# print(mylist[0][1])
+
+#31
+# myTuple=('ali','ahmadi',18)
+# print(myTuple)
+# print(*myTuple)
+# print(myTuple[1])
+# print(myTuple.count('ali'))
+# print(myTuple.index(18))
+# # print(myTuple.index(20))#error
+# # myTuple[0]='reza'#error
+# print(myTuple)
+
+#32
+# name=input('enter name : ')
+# age=int(input('enter age : '))
+# myTuple=(name,age)
+# mylist=[]
+# mylist.append(myTuple)
+# print(mylist)
+
+#33
+# myDict={'name':'sara','phone':'0911'}
+# print(myDict)
+# print(*myDict)#keys
+# print(myDict['name'])
+# myDict['phone']='0930'
+# myDict['age']=24
+# del myDict['name']
+# print(myDict)
+# mylist=[myDict]
+# print(mylist)
+# print(mylist[0])
+# print(mylist[0]['phone'])
+
+#34
+# setA={5,8,3,12}
+# setB={10,8,4,9}
+# print(setA.union(setB))
+# print(setA.intersection(setB))
+# print(setA.difference(setB))
+# print(setB.difference(setA))
+# setA.update({5,9,12,-3})
+# print(setA)
+# setA.remove(5)
+# setA.add(10)
+# print(setA)
+# print(list(setA)[1])
+# print(set([7,3,2]))
+
+#35
+# setA={5,8,3,12}
+# setB={10,8,4,9}
+
+# print(setA.union(setB).difference(setA.intersection(setB)))
+# U=setA.union(setB)
+# I=setA.intersection(setB)
+# print(U.difference(I))
+
+# # | -> or , & -> and
+# print((setA | setB) - (setA & setB))
+
+#36
+# mydict={'name':'ava','age':17}
+# print(mydict.get('name'))
+# print(mydict.values())
+# print(mydict.items())
+# print(mydict.keys())
+
+#37
+# setA={'amir','reza','sara'}
+# setB={'ava','reza','ali'}
+# print(setA)
+# print(setA.intersection(setB))
+
+#38
+# print(tuple(['sara','asadi',21]))
+# print(dict([['name','sara'],['age',21]]))
+# print(dict([('name','sara'),('age',21)]))
+# print(bool(0))
+# print(bool(1))
+# print(bool(5))
+# print(bool(-3))
+# print(bool(2.25))
+# print(bool('maharat'))
+# print(bool(''))
+# print(bool([]))
+# print(bool([5,9,8]))
+# print(type('maharat'))
+# print(type(25))
+# print(type(2.5))
+# print(type([5,12]))
+# print(type((5,12)))
+# print(list({'name':'sara','age':21}))#?!
+
+#39
+# mylist=[7,9,-12,5,8,9,-5]
+# print(mylist.count(9))
+# print(mylist.count(-5))
+# print(mylist.count(10))
+# #lambda
+# # print(mylist.count(lambda n:n<0))#?1
+# f=filter(lambda n : n<0 , mylist)
+# print(list(f))
+
+#40
+# mylist=[12,-5,11,8,323,112,-2,10]
+# result=filter(lambda n: n>=10, mylist)
+# print(result)
+# print(*result)
+# result=filter(lambda a: a%2==0,mylist)
+# print(*result)
+# result=filter(lambda n: n>=10 and n<=99 ,mylist)
+# print(*result)
+
+#41
+# names=['sara','amir','reza','ava','narges']
+# i=input('enter search : ')
+# result=filter(lambda nam: i in nam ,names)
+# # result=filter(lambda nam: i not in nam ,names)
+# # print(*result)
+# # print(list(result))
+# filterList=list(result)
+# print(filterList)
+# print(*filterList)
+
+#42
+# for i in range(1,100):
+#     if i%2==0:
+#         print(i,end='\t')
+
+#43
+myStr='I Love Python'
+print(myStr.lower())
+print(myStr.upper())
+#1 -> I Lve Pythn
+#2 -> I LoVe pYtHoN
+
+for n in myStr:
